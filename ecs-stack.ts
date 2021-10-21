7c985b7acac5da4244709f7cc3dc774bf31a4100
Dmitry Losev <dmitrii@powerodit.com>
2021-10-21 22:09:20 +0700

    CDK config updated

---
+++ ecs-stack.ts
@@ -0,0 +1,105 @@
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as cr from "@aws-cdk/custom-resources";
+
+export class FargateStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, time: number, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const taskDefinitionArn = process.env.TASK_DEFINITION_ARN;
+
+        if (!taskDefinitionArn) {
+            throw new Error('TASK_DEFINITION_ARN environment variable is missing');
+        }
+
+        const vpc = new ec2.Vpc(this, appendPostfix("ecs-vpc"), {
+            natGateways: 1,
+            subnetConfiguration: [
+                {
+                    name: appendPostfix('public-subnet'),
+                    subnetType: ec2.SubnetType.PUBLIC
+                }
+            ]
+        });
+
+        const cluster = new ecs.Cluster(this, appendPostfix("Cluster"), {
+            vpc: vpc
+        });
+
+        const securityGroupName = appendPostfix("securityGroup");
+        const securityGroup = new ec2.SecurityGroup(this, securityGroupName, {
+            allowAllOutbound: true,
+            securityGroupName: securityGroupName,
+            vpc: vpc
+        });
+        securityGroup.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
+
+        // Dummy task definition, will be replaced by ARN of the existing task definition
+        const dummyTaskDefinition = new ecs.TaskDefinition(this, "dummyTask", {
+                compatibility: ecs.Compatibility.FARGATE,
+                cpu: "256",
+                memoryMiB: "512",
+            }
+        );
+        dummyTaskDefinition.addContainer("dummyContainer", {
+            image: ecs.ContainerImage.fromRegistry("dummyImage"),
+        });
+
+        const service = new ecs.FargateService(this, "service", {
+            cluster,
+            desiredCount: 1,
+            taskDefinition: dummyTaskDefinition,
+            securityGroups: [securityGroup],
+            assignPublicIp: true
+        });
+
+        const taskName = createCustomResource(this, "taskName", "ECS", "listTasks", "taskArns.0", {
+            "cluster": cluster.clusterArn
+        });
+
+        const eniId = createCustomResource(this, "eniId", "ECS", "describeTasks", "tasks.0.attachments.0.details.1.value", {
+            "cluster": cluster.clusterArn,
+            "tasks": [taskName.getResponseField("taskArns.0")]
+        });
+
+        const publicIp = createCustomResource(this, "publicIp", "EC2", "describeNetworkInterfaces", "NetworkInterfaces.0.Association.PublicIp", {
+            NetworkInterfaceIds: [
+                eniId.getResponseField("tasks.0.attachments.0.details.1.value")
+            ]
+        });
+
+        new cdk.CfnOutput(this, 'publicIp', {
+            value: publicIp.getResponseField("NetworkInterfaces.0.Association.PublicIp")
+        });
+
+        // https://github.com/aws/aws-cdk/issues/6240
+        (service.node.tryFindChild("Service") as ecs.CfnService)?.addPropertyOverride(
+            "TaskDefinition",
+            taskDefinitionArn
+        );
+
+        function appendPostfix(name: string): string {
+            return `${name}-${time}`;
+        }
+
+        function createCustomResource(scope: cdk.Construct, name: string, service: string, action: string, path: string, parameters?: any): cr.AwsCustomResource {
+            return new cr.AwsCustomResource(
+                scope,
+                appendPostfix(name),
+                {
+                    onCreate: {
+                        service: service,
+                        action: action,
+                        outputPaths: [path],
+                        parameters: parameters,
+                        physicalResourceId: cr.PhysicalResourceId.fromResponse(path)
+                    },
+                    policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
+                        resources: cr.AwsCustomResourcePolicy.ANY_RESOURCE,
+                    })
+                }
+            );
+        }
+    }
+}
