ec0060e7236a2a3edb63c27e40e6adbc566f8673
Dmitry Losev <dmitrii@powerodit.com>
2021-10-26 14:49:50 +0700

    Home page text changed

--- run.sh
+++ run.sh
@@ -4,8 +4,12 @@ composer require symfony/twig-bundle
 
 cd "${SYMFONY_PROJECT_NAME}"
 
+echo '---------Initializing database start---------'
+
 php bin/console doctrine:database:create
 php bin/console doctrine:schema:update --force
 #php bin/console zf:fixtures:load
+
+echo '---------Initializing database end---------'
 
 php -S 0.0.0.0:$PORT -t "$WEB_DIR"
